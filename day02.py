2b2d28bb214ecaccf18ae0e1348ce93a8f0e2bb7
ddssz033 <ddssz033@gmail.com>
2024-01-15 10:44:53 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,17 @@
+univ = "Inha univ"
+print(univ)
+print(univ[5])
+# univ[5] = 'U" //immutable
+subjects = ['python', 'c++', 'linux', 'datastructure', 'db']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'datastructure & algorithm' # mutable
+print(subjects)
+# print(0.1)
+# print(1e-1)
+# print(0.01)
+# print(1e-2)
+# print(3.14)
+# print(314e-2)
+# print(21000)
+# print(21_000)
