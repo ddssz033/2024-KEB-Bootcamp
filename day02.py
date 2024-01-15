aaa38454b16312a60864f7a8316000cf006bed79
ddssz033 <ddssz033@gmail.com>
2024-01-15 15:47:40 +0900

    v1.3 python day02

--- day02.py
+++ day02.py
@@ -1,12 +1,8 @@
-# (100°F − 32) × 5/9 = 37.778°C
-# (0°C × 9/5) + 32 = 32°F
-
-menu = input("1) fahrenheit -> celsius 2) celsius -> fahrenheit 3) quit program : ")
-if menu == '1':
-    fahrenheit = float(input('input fahrenheit : '))
-    print(f'Fahrenheit: {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5/9):.4f}C')
-elif menu == '2':
-    celsius = float(input('input celsius : '))
-    print(f'celsius: {celsius}C, fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+letter = input('input alphabet letter : ')
+# vowels = ['a', 'e', 'i', 'o', 'u'] #set
+vowels = "aeiou" #str
+print(type(vowels))
+if letter in vowels:
+    print(f'{letter} is a vowel')
 else:
-    print('terminate program')
+    print(f'{letter} is a consonant')
