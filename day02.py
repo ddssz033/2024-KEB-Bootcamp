aed43b731ff3fdf0700433f267a1419c9ae334e6
ddssz033 <ddssz033@gmail.com>
2024-01-15 11:51:12 +0900

    v0.6 python day02

--- day02.py
+++ day02.py
@@ -1,36 +1,11 @@
-univ = "Inha univ"
-print(univ)
-print(univ[5])
-# univ[5] = 'U" //immutable
-subjects = ['python', 'c++', 'linux', 'datastructure', 'db']
-print(subjects)
-print(subjects[3])
-subjects[3] = 'datastructure & algorithm' # mutable
-print(subjects)
-# print(0.1)
-# print(1e-1)
-# print(0.01)
-# print(1e-2)
+base_number = int(input('input base number : '))
+exponent_number = int(input('input exponent number : '))
+print(f'밑은 {base_number}, 지수는{exponent_number}, 결과값은 {base_number ** exponent_number}')
 
-#파이썬 키워드들은 변수명으로 사용 불가
-#case-sensitive
-# ok
-abc = 7
-Abc = 8
-ABC = 6
-print(abc, Abc, ABC)
-
-test9 = 77
-# 9test = tt 변수명 숫자로는 시작 못함
-_9test = 11
-print(test9, _9test)
-
-# False = 123 안된다, reserved words
-
-print(type(3.14))
-print(isinstance(3.14, float))
-
-artists = ["BTS", "Newjeans", "ive", "nmixx", "aespa"]
-groups = artists
-artists[2] = "seventeen"
-print(artists, groups)
+# print(type(3.14))
+# print(isinstance(3.14, float))
+#
+# artists = ["BTS", "Newjeans", "ive", "nmixx", "aespa"]
+# groups = artists
+# artists[2] = "seventeen"
+# print(artists, groups)
