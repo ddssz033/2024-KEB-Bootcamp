1b1a1c85b5dc61f29085b37b30b4fa346ee62305
ddssz033 <ddssz033@gmail.com>
2024-01-15 14:56:38 +0900

    v1.1 python day02

--- day02.py
+++ day02.py
@@ -1,3 +1,10 @@
 # (100°F − 32) × 5/9 = 37.778°C
-fahrenheit = float(input('input fahrenheit : '))
-print(f'Fahrenheit: {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5/9):.4f100}C')
+# (0°C × 9/5) + 32 = 32°F
+
+menu = input("1) fahrenheit -> celsius 2) celsius -> fahrenheit 3) quit program : ")
+if menu == '1':
+    fahrenheit = float(input('input fahrenheit : '))
+    print(f'Fahrenheit: {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5/9):.4f}C')
+elif menu == '2':
+    celsius = float(input('input celsius : '))
+    print(f'celsius: {celsius}C, fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
