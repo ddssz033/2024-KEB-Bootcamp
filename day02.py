c7a28c6d9acf3cead67660ed68d489d392f2af8f
ddssz033 <ddssz033@gmail.com>
2024-01-15 14:15:07 +0900

    v0.9 python day02

--- day02.py
+++ day02.py
@@ -1,10 +1,8 @@
-# first_number = int(input("first number : "))
-# second_number = int(input("second number : "))
-# quotient = first_number // second_number
-# remainder = first_number // second_number
-# print(f'몫은 {quotient} 나머지는 {remainder}입니다')
-
-first_number = int(input("first number : "))
-second_number = int(input("second number : "))
-print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다')
-#divmod의 첫번째는 몫([0]), 두번째는 나머지([1])
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'), ord('A'), ord('2'))
