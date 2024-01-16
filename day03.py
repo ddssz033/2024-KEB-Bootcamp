c2aeb554fb5ab4fa512ea08d94551d1c293c9b67
ddssz033 <ddssz033@gmail.com>
2024-01-16 11:39:53 +0900

    v0.4

--- day03.py
+++ day03.py
@@ -1,15 +1,6 @@
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-print(course.find('KEB')) #마이너스가 뜰 경우 찾지 못함
-print(course.rfind('KEB')) #뒤부터 찾는다
-print(course.find('Inha')) #-1
-# print(course.index('Inha')) #valueerror
-print(course)
-course = course.replace('KEB', 'Inha', 1)
-print(course)
-print(course.strip())
-print(course.strip("!#.*"))
-# print(course)
-# course = course.replace('KEB', "Inha")
-# print(course)
-
-#strip: 불필요한 공백을 없애줌, 특정 문자를 없앨수도 있다, 양쪽 끝만 제거
+subjects = "python c++ database linux"
+subject = input("과목 입력: ")
+if subjects.find(subject) != -1: #찾은 경우(값이 -1이 아닌경우)
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다")
+else:
+    print('해당 과목이 없습니다')
