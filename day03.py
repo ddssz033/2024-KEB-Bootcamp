fadc995f8535a06a9e200330f562b3009788f7f5
ddssz033 <ddssz033@gmail.com>
2024-01-16 15:34:57 +0900

    v0.8 loop

--- day03.py
+++ day03.py
@@ -1,36 +1,19 @@
-univ = "inha"
-i = 0
-while i < len(univ):
-    print(univ[i], end=' ')
-    i = i + 1
+#두개의 숫자 사이 소수들
+numbers = input("input number : ").split()
 
-print()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1 #왼쪽이 더 큰 경우 swapping
 
-for letter in univ: #오류가능성이 적고 코드가 간단하다
-    print(letter, end=' ') #end는 글자 옆으로 나오게 하는 용도
+for number in range(n1, n2+1):
+    is_prime = True  # int -> bool
 
-# 특정 구간만 제어하고 싶다 -> while이 좋다(시작점, 인덱스 등 조절 용이)
-# 전부 출력하고 싶다 -> for이 좋다(range로 구간 조절 가능함)
-
-print()
-
-for k in range(0, len(univ)): #0부터 univ의 길이까지
-    print(univ[k], end=' ')
-
-#primenumber for ver
-#prime number
-number = int(input("input number : "))
-is_prime = True #int -> bool
-
-if number < 2:
-  print(f'{number} is not prime number')
-else:
-  for i in range(2, number):
-    if number % 1 == 0:
-      is_prime = False
-      break
-
-  if is_prime:
-    print(f'{number} is prime number')
-  else:
-    print(f'{number} is not prime number')
+    if number < 2:
+        pass
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime: print(number, end=' ')
