9a51e69e7abba08e9bd15f98077139abdcb1c56b
ddssz033 <ddssz033@gmail.com>
2024-01-16 14:37:11 +0900

    v0.5 prime number

--- day03.py
+++ day03.py
@@ -1,13 +1,14 @@
 #prime number
 number = int(input("input number : "))
-cnt = 0
-i = 1
-while i <= number:
+is_prime = True #int -> bool
+i = 2
+while i < number:
     if number % i == 0:
-        cnt = cnt + 1
+        is_prime = False # +을 제거함
+        break
     i = i + 1
 
-if cnt == 2: #나누어 떨어지는 수가 1과 자기 자신뿐인 경우
+if is_prime: # ==을 제거
     print(f'{number} is prime number')
 else: #그외(소수가 아님)
     print(f'{number} is not prime number')
