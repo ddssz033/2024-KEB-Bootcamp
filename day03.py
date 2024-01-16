1e21f0abd907eeeeffd65ed850d86a3fe5ca5be7
ddssz033 <ddssz033@gmail.com>
2024-01-16 14:59:30 +0900

    v0.6 loop

--- day03.py
+++ day03.py
@@ -1,19 +1,18 @@
-#prime number
-number = int(input("input number : "))
-is_prime = True #int -> bool
-i = 2
-while i < number:
-    if number % i == 0:
-        is_prime = False # +을 제거함
-        break
+univ = "inha"
+i = 0
+while i < len(univ):
+    print(univ[i], end=' ')
     i = i + 1
 
-if is_prime: # ==을 제거
-    print(f'{number} is prime number')
-else: #그외(소수가 아님)
-    print(f'{number} is not prime number')
+print()
 
+for letter in univ: #오류가능성이 적고 코드가 간단하다
+    print(letter, end=' ') #end는 글자 옆으로 나오게 하는 용도
 
+# 특정 구간만 제어하고 싶다 -> while이 좋다(시작점, 인덱스 등 조절 용이)
+# 전부 출력하고 싶다 -> for이 좋다(range로 구간 조절 가능함)
 
-# subjects = {'python':'kim', 'c++': 'sung', 'datastructure': 'kim', 'db': 'kang'}
-# print("{0[python]} {0[datastructure]}".format(subjects))
+print()
+
+for k in range(0, len(univ)): #0부터 univ의 길이까지
+    print(univ[k], end=' ')
