0b30089591d84926891693915a523e6517311d90
ddssz033 <ddssz033@gmail.com>
2024-01-17 16:13:42 +0900

    v0.8

--- day04.py
+++ day04.py
@@ -1,21 +1,28 @@
-sugang = dict(python="kim", cpp="sung", db="kang")
-# print(sugang)
-# sugang['datastructure'] = 'kim' #추가
-# print(sugang)
-# sugang['datastructure'] = 'park' #update
-# print(sugang)
-# print(sugang['db'])
-# print(sugang.get('db'))
-# print(sugang.get('opensource'))
-# print(sugang.get('opensource', 'not exist')) #none 대신 not exist 뜨게 만들기
-for subject, professor in sugang.items(): #subject = key, professor = values
-    print(f'{subject} 과목 담당교수는 {professor}입니다')
+import random
 
-for k in sugang.keys():
-    print(k)
+drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삼겹살", "고량주": "양꼬치"}
 
-for v in sugang.values():
-    print(v)
+# print(random.choice(drinks_foods_keys))
+japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
+drinks_foods.update(japan_drinks_foods)
+drinks_foods_keys = list(drinks_foods)
 
-for s in sugang.items(): #tuple 형태로 출력
-    print(s)
+
+while True:
+    menu = input(f'다음 술 중에 고르세요.\n1) {drinks_foods_keys[0]}, 2) {drinks_foods_keys[1]}, 3) {drinks_foods_keys[2]}, 4) {drinks_foods_keys[3]}, 5) {drinks_foods_keys[4]} 6) 아무거나 7) 종료: ')
+    if menu == '1':
+        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
+    elif menu == '2':
+        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
+    elif menu == '3':
+        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
+    elif menu == '4':
+        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
+    elif menu == '5':
+        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
+    elif menu == '6':
+        random_drink = random.choice(drinks_foods_keys)
+        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
+    elif menu == '7':
+        print(f'다음에 또 오세요')
+        break
