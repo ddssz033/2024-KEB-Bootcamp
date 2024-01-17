31ce3aba42f2e37cef6d9a6f6adfdb411d3e1e03
ddssz033 <ddssz033@gmail.com>
2024-01-17 13:34:19 +0900

    v0.5

--- day04.py
+++ day04.py
@@ -1,11 +1,11 @@
-subjects = ["c++", "5", "java", "python", "java", "9", "데이터베이스", "리눅스"]
-subjects = subjects[::-1]
-print(subjects)
-#지울때는 del이나 pop을 쓰자, remove는 피하자
-# subjects.pop(2)
-# print(subjects)
-# subjects.sort(reverse = True)
-print(subjects)
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
+import copy
+
+subjects = ["a", ["b", "c"], "d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e)
