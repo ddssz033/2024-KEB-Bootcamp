560fd04d7e0fddaeeccd37c20affedb40d1efbff
ddssz033 <ddssz033@gmail.com>
2024-01-18 14:22:00 +0900

    v0.6

--- day05.py
+++ day05.py
@@ -1,9 +1,2 @@
-def squares(*n) -> list:
-
-    return [pow(i, 2) for i in n]
-
-def run_function(f, *number) -> list:
-    return f(*number)
-
-print(squares(7, 5, 2))
-print(run_function(squares, 9))
+numbers = ["7", "-11", "3"]
+print(sum(map(int, numbers)))
