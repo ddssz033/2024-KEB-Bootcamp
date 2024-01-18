7b1534141232951ff8f4c320ce0fe1cf5ce43436
ddssz033 <ddssz033@gmail.com>
2024-01-18 13:37:55 +0900

    v0.5 function

--- day05.py
+++ day05.py
@@ -1,56 +1,9 @@
-def isprime(n) -> bool:
-    if n < 2:
-        return False
-    else:
-        i = 2
-        while i*i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
+def squares(*n) -> list:
 
+    return [pow(i, 2) for i in n]
 
+def run_function(f, *number) -> list:
+    return f(*number)
 
-# Assignment (add prime series program)
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
-
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        is_prime = True
-
-        if number < 2:
-            print(f'{number} is NOT prime number!')
-        else:
-            for i in range(2, number):
-                if number % i == 0:
-                    is_prime = False
-                    break
-
-            if is_prime:
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            if isprime(number):
-                print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
+print(squares(7, 5, 2))
+print(run_function(squares, 9))
