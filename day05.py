3b0c399df3a086c126879c01d56dcf398cdd19fa
ddssz033 <ddssz033@gmail.com>
2024-01-18 09:25:14 +0900

    v0.2 assignment day05

---
+++ day05.py
@@ -0,0 +1,13 @@
+# univ = ("inha university")
+# counts_alphabet = {letter: univ.count(letter) for letter in univ} #dictionary comprehension
+# print(counts_alphabet)
+
+# univ = ("inha university")
+# counts_alphabet = dict()
+# for letter in univ:
+#     counts_alphabet[letter] = univ.count(letter)
+# print(counts_alphabet)
+
+#assignment 8.10
+squares = {n: pow(n, 2) for n in range(10)}
+print(squares)
