37a6790ce60de8bedf614de0d16f28c8ffa8a995
ddssz033 <ddssz033@gmail.com>
2024-01-19 13:04:47 +0900

    v0.3

--- day06.py
+++ day06.py
@@ -1,15 +1,5 @@
-import random
-numbers = [random.randint(1, 100) for i in range(5)]
-print(numbers)
+class Pokemon:
+    def __init__(self, name):
+        print(f'{name} 포켓몬스터 생성')
 
-try:
-    pick = int(input(f"input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-except IndexError as err:
-    print(f"wrong idx number\n{err}")
-except ValueError as err:
-    print(f"input only number\n{err}")
-except ZeroDivisionError as err:
-    print(f"the denominator cannot be 0. \n{err}")
-except Exception: #가장 범위가 넓은 에러는 가장 밑으로
-    print('error occurs')
+pikachu = Pokemon("피카츄")
