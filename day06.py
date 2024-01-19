d1a25b42956790bc3dc6706ab0ba81706263c3df
ddssz033 <ddssz033@gmail.com>
2024-01-19 13:44:58 +0900

    v0.4

--- day06.py
+++ day06.py
@@ -1,5 +1,14 @@
 class Pokemon:
     def __init__(self, name):
+        self.name = name
         print(f'{name} 포켓몬스터 생성')
 
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+
 pikachu = Pokemon("피카츄")
+squietle = Pokemon("꼬부기")
+charizard = Pokemon("리자몽")
+print(pikachu.name)
+print(squietle.name)
+charizard.attack(squietle)
