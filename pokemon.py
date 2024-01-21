e1e70a419686cc2e6134a897dd39feb20ba5bcf8
ddssz033 <ddssz033@gmail.com>
2024-01-21 22:19:31 +0900

    v0.2 game

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,11 @@
 import random
 class Starting_Pokemon:
-    def Username(self):
+    def __init__(self):
+        self.username = self
+    def username(self):
+        username = input("트레이너의 이름을 입력하세요: ")
+        print(f'{username}, 환영합니다!')
+    def choosepokemon(self):
+        starting = ['모부기', '팽도리', '불꽃숭이']
+
 
